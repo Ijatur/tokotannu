c2c7607e11baef3d87958ad95bbac4f379aa74c3
Ijat <turamadhanizza@gmail.com>
2021-09-19 22:42:36 +0700

    bikin layout tp blm bisa pindah

--- benran/app/src/main/java/com/example/tokozakaria/MainActivity.java
+++ benran/app/src/main/java/com/example/tokozakaria/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.tokozakaria;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public CardView barang,penjualan,keuntungan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        barang = findViewById(R.id.barang);
+        penjualan = findViewById(R.id.penjualan);
+        keuntungan = findViewById(R.id.keuntungan);
+
     }
+
 }
