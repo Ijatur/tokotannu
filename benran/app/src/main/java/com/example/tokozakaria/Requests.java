f19389007ae06d07820d59d978829d10e4855e21
tannu wibowo <tannuwibowo25@gmail.com>
2021-09-25 19:45:22 +0700

    create firebase

---
+++ benran/app/src/main/java/com/example/tokozakaria/Requests.java
@@ -0,0 +1,67 @@
+package com.example.tokozakaria;
+
+public class Requests {
+    public String ID;
+    public String nama;
+    public String harga;
+    public String stock;
+    public String sold;
+    public String supplier;
+    public String email;
+
+    public String getSold() {
+        return sold;
+    }
+
+    public void setSold(String sold) {
+        this.sold = sold;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getHarga() {
+        return harga;
+    }
+
+    public void setHarga(String harga) {
+        this.harga = harga;
+    }
+
+    public String getStock() {
+        return stock;
+    }
+
+    public void setStock(String stock) {
+        this.stock = stock;
+    }
+
+    public String getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(String supplier) {
+        this.supplier = supplier;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
